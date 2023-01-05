f950d6f02ea55c94f6850d4ccd1a63797313b683
c-livi <121881563+c-livi@users.noreply.github.com>
2023-01-05 13:41:31 -0500

    Update scanlib.rs

--- scanlib.rs
+++ scanlib.rs
@@ -1,1 +1,12 @@
+use std::fs;
+use std::path::Path;
 
+#[pyfunction]
+fn scan_directory(path: &str) -> Vec<String> {
+    let mut entries = Vec::new();
+    for entry in fs::read_dir(Path::new(path)).unwrap() {
+        let entry = entry.unwrap();
+        entries.push(entry.path().to_str().unwrap().to_owned());
+    }
+    entries
+}
